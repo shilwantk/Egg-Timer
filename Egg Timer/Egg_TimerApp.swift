4020e9726064a4d31f019e43413f3a915a575eda
Kirti Shilwant <shilwantkirti@gmail.com>
2025-02-01 20:00:50 +0530

    Initial Commit

---
+++ Egg Timer/Egg_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_TimerApp.swift
+//  Egg Timer
+//
+//  Created by KirtiS on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Egg_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Egg TimerTests/Egg_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_TimerTests.swift
+//  Egg TimerTests
+//
+//  Created by KirtiS on 2/1/25.
+//
+
+import Testing
+@testable import Egg_Timer
+
+struct Egg_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
